66196d0fa77b2a90b0c8ee0b07ab3621c76cf13f
root <root@localhost>
2023-11-16 17:58:14 +0800

    auto publish 1700128694435

--- src/main.c
+++ src/main.c
@@ -1,15 +1,10 @@
 #include<stdio.h>
 
-int add(int number1, int number2){
-     int result;
-     result= number1 + number2;
-     return result;
+void hello(){
+      printf("Hello Programming Hero");
 }
+
 int main(){
-    int number1, number2, result;
-    printf("Enters two numbers you wish to add:");
-    scanf("%d %d",&number1,&number2);
-    result = add(number1, number2);    // function call
-    printf("Addition is %d",result);
-    return 0;
+     hello();
+     return 0;   
 }
