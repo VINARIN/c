08d41a7a7cb7dc4d08099b3fab6e3be0ba80be12
root <root@localhost>
2023-11-18 21:02:18 +0800

    auto publish 1700312538230

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
-#include <stdio.h> 
-int globalNum;
-int main () {
-  int num1, num2;
-  num1 = 10;
-  num2 = 20;
-  globalNum = num1 + num2;
-  printf ("value of num1 = %d, num2 = %d and globalNum = %d\n", num1, num2, globalNum);
-  return 0;
+#include <stdio.h>
+void main()
+{
+       int num = 5;
+        if(num > 0){
+        printf("Number is positive");
+    }
+    else{
+        printf("The number is not positive ");
+    }    
 }
